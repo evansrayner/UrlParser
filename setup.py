614a6407a90aca1dbfcc313e375bdf9947fd3f66
Ben Evans <benchristianevans@gmail.com>
2017-05-21 22:11:44 +0100

    Added Requirements and updated setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='UrlParser',
+    version='0.1dev',
+    packages=['urlparser',],
+    license='GNU GENERAL PUBLIC LICENSE',
+    long_description=open('README.txt').read(),
+)
